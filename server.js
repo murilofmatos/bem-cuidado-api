4a91c56731fca35ac27cf993cc6333c77fc73705
Murilo Francisco Matos <130021023+murilofmatos@users.noreply.github.com>
2025-05-22 18:14:20 -0300

    fix logger

--- server.js
+++ server.js
@@ -207,6 +207,7 @@ app.use(express.json());
 app.use(cors());
 app.use((req, res, next) => {
   console.log(`Request: ${req.method} : ${req.url}`);
+  next();
 });
 
 app.get("/", async (req, res) => {
